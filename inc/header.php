0edaf682d926a824689f92e66fd6381b19fc14eb
Anthony Fox <anthonyfox@anthonysmbp936.rowanads.rowan.edu>
2018-11-27 12:42:33 -0500

    further detail branch

--- inc/header.php
+++ inc/header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $pageTitle; ?> | MovieFox</title>
+    <title><?php echo $pageTitle; ?> | MovieFox Test</title>
 </head>
 <body>
